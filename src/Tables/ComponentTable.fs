670285bb9fc80d1f5e4249c0c139ba0e96305b27
my0n <my0n@protonmail.com>
2019-11-16 11:53:17 -0600

    feat: group components by type

--- src/Tables/ComponentTable.fs
+++ src/Tables/ComponentTable.fs
@@ -6,7 +6,25 @@ open Bulma.Table
 open Comp.ShipComponent
 open Fable.React
 
-let render comps model dispatch =
+let render (comps: ShipComponent list) model dispatch =
+    let groupedComps =
+        comps
+        |> List.groupBy (function
+            | comp when comp.BuiltIn -> "Built-In Components"
+            | Engine _               -> "Engines"
+            | Magazine _             -> "Magazines"
+            | PowerPlant _           -> "Power Plants"
+            | _                      -> "Other"
+        )
+        |> List.sortBy (fun (name, _) -> name)
+        |> List.map (fun (name, comps) ->
+            name,
+            comps
+            |> List.sortBy (fun comp ->
+                comp.Name.ToLowerInvariant()
+            )
+        )
+
     let addComponent ship (comp: ShipComponent) =
         match comp.BuiltIn with
         | true -> Msg.CopyComponentToShip (ship, comp)
@@ -14,32 +32,36 @@ let render comps model dispatch =
         |> dispatch
     let deleteComponent comp = Msg.RemoveComponent comp |> dispatch
 
-    let componentListOptions: ColumnOptions<ShipComponent> list =
-        [
-            {
-                Name = "Name"
-                Render = fun comp -> str comp.Name
-            }
-            {
-                Name = ""
-                Render = fun comp ->
-                    Bulma.FC.Button
-                        "Add"
-                        Bulma.FC.ButtonOpts.Empty
-                        (
-                            match model.CurrentShip with
-                            | Some ship -> fun _ -> addComponent ship comp
-                            | None -> id
-                        )
-            }
-            {
-                Name = ""
-                Render = fun comp ->
-                    Bulma.FC.Button
-                        "Delete"
-                        (match Model.canDeleteComponent model comp with true -> Bulma.FC.ButtonOpts.Empty | false -> Bulma.FC.ButtonOpts.Disabled)
-                        (fun _ -> deleteComponent comp)
-            }
-        ]
-
-    Bulma.Table.render componentListOptions comps None ignore
+    groupedComps
+    |> List.map (fun (name, comps) ->
+        let componentListOptions: ColumnOptions<ShipComponent> list =
+            [
+                {
+                    Name = name
+                    Render = fun comp -> str comp.Name
+                }
+                {
+                    Name = ""
+                    Render = fun comp ->
+                        Bulma.FC.Button
+                            "Add"
+                            Bulma.FC.ButtonOpts.Empty
+                            (
+                                match model.CurrentShip with
+                                | Some ship -> fun _ -> addComponent ship comp
+                                | None -> id
+                            )
+                }
+                {
+                    Name = ""
+                    Render = fun comp ->
+                        Bulma.FC.Button
+                            "Delete"
+                            (match Model.canDeleteComponent model comp with true -> Bulma.FC.ButtonOpts.Empty | false -> Bulma.FC.ButtonOpts.Disabled)
+                            (fun _ -> deleteComponent comp)
+                }
+            ]
+            
+        Bulma.Table.render componentListOptions comps None ignore
+    )
+    |> fragment []
